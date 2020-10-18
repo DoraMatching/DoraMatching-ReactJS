f0d734979d825f900b8a9bc476fba97e674b9a57
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-10-19 00:40:10 +0700

    add navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css'
 import Layout from "../components/Layout";
 
+
 function MyApp({ Component, pageProps }) {
   return <Layout> <Component {...pageProps} /></Layout>
 }

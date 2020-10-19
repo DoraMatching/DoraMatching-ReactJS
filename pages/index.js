1fc244b0fe7277aaf7786ca7250631b5284b4fea
hongquanhak <hongquanhak@gmail.com>
2020-10-19 16:23:31 +0700

    add blog component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { Grid } from "semantic-ui-react";
+import Post from "../components/Posts/Post";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Grid divided="vertically">
+          <Grid.Column width={8}>
+            <Post />
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <div>Quan</div>
+          </Grid.Column>
+      </Grid>
     </div>
-  )
+  );
 }

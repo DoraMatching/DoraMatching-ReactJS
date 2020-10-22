4ec7e0654a4161ab73a3fe44734b45ae994d3913
tranphuquy19 <christian@enclave.vn>
2020-10-22 17:14:41 +0700

    add Question page

---
+++ components/QuestionsPage/CardQuestionCenter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CardQuestionCenter extends Component {
+  render() {
+    return (
+      <div>
+        center
+      </div>
+    )
+  }
+}

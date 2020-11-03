c7cbc212c5249099cc9b18cc16ac93af441b57e5
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-11-04 00:32:34 +0700

    fix it

--- components/Schedule/fullcalendar.js
+++ components/Schedule/fullcalendar.js
@@ -1,6 +1,6 @@
+import React from "react";
 import dynamic from "next/dynamic";
 import { useEffect, useState } from "react";
-import React from "react";
 
 let CalendarComponent;
 
--- components/TopicPage/CardTopicsRight.js
+++ components/TopicPage/CardTopicsRight.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import FullCalendar from '../Schedule/fullcalendar'
 
 export default function CardTopicsRight() {
   return (
     <div>
-      Calendar
+      <FullCalendar />
     </div>
   )
 }

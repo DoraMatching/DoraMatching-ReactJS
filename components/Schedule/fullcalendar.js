cd00e965c1f3ebc466caf4dd2fbb9e0bd59e0b89
tranphuquy19 <christian@enclave.vn>
2020-11-04 17:01:18 +0700

    call api tag-question

--- components/Schedule/fullcalendar.js
+++ components/Schedule/fullcalendar.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import React, { useEffect, useState } from "react";
 
 let CalendarComponent;
 
---
+++ pages/tag-question/index.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React from 'react';
+import TagQuestionsPage from '../../containers/TagQuestionPage';
+import styles from '../../styles/Home.module.css'
+
+function tagQuestions({tagQuestions}) {
+  return (
+    <div className={styles.container}>
+      <TagQuestionsPage tagQuestions={tagQuestions} />
+    </div>
+  );
+}
+
+tagQuestions.getInitialProps = async () => {
+  const {data} = await axios.get('https://api.dev.doramatching.tk/tag-question')
+  return { tagQuestions: data.items };
+}
+
+export default tagQuestions;

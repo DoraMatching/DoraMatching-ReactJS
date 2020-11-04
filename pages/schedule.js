a69d5c5e4a302a6030f3720db731a73d7372d872
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-11-05 02:33:56 +0700

    fix schedule

--- pages/schedule.js
+++ pages/schedule.js
@@ -1,11 +1,11 @@
 import React from "react";
-import CardSchedule from "../components/Schedule/fullcalendar";
+import Schedule from "../components/Schedule/Schedule";
 import styles from "../styles/Home.module.css";
 
 function topics() {
   return (
     <div className={styles.container}>
-      <CardSchedule />
+      <Schedule />
     </div>
   );
 }

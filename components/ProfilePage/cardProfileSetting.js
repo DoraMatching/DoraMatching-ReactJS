6cc2bc3579a87466c5c6a1450e155d1bfa0abe1d
Tran Van Hong Quan <hongquanhak@gmail.com>
2020-11-18 00:25:20 +0700

    add change profile

--- components/ProfilePage/cardProfileSetting.js
+++ components/ProfilePage/cardProfileSetting.js
@@ -1,79 +1,114 @@
-import React from 'react';
-import { Button, Form, Input, Tab } from 'semantic-ui-react';
+import React, { useEffect, useState } from "react";
+import { Button, Form, Input, Tab } from "semantic-ui-react";
+import { useAuth } from "../../contexts/auth";
 
 const fileInputRef = React.createRef();
 
-const panes = [
-  {
-    menuItem: "Change Profile",
-    render: () => (
-      <Tab.Pane attached={false}>
-        <Form unstackable>
-          <Form.Group widths={2}>
-            <Form.Input
-              label="Email"
-              placeholder="email"
-              value="abc"
-              disabled
-            />
-            <Form.Input
-              label="Username"
-              placeholder="username"
-              value="abc"
-              disabled
-            />
-          </Form.Group>
-          <Form.Group widths={2}>
-            <Form.Input label="Name" placeholder="name" />
-            <Form.Input label="Phone number" placeholder="phone" />
-          </Form.Group>
-          <Form.Field>
-            <Button
-              content="Choose Image"
-              labelPosition="left"
-              icon="image"
-              onClick={() => fileInputRef.current.click()}
-            />
-            <input
-              ref={fileInputRef}
-              type="file"
-              hidden
-              // onChange={this.fileChange}
-            />
-          </Form.Field>
-          <Button color="linkedin" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Tab.Pane>
-    ),
-  },
-  {
-    menuItem: "Change Password",
-    render: () => (
-      <Tab.Pane attached={false}>
-        <Form>
-          <Form.Field required>
-            <label>Old Password</label>
-            <Input placeholder="old pass" required />
-          </Form.Field>
-          <Form.Field required>
-            <label>New Password</label>
-            <Input placeholder="new pass" required />
-          </Form.Field>
-          <Form.Field required>
-            <label>Repeat New Password</label>
-            <Input placeholder="repeat new pass" required />
-          </Form.Field>
-          <Button color="linkedin" type="submit">
-            Confirm
-          </Button>
-        </Form>
-      </Tab.Pane>
-    ),
-  },
-];
+const CardProfileSetting = ({ userAcc }) => {
+  const { user } = useAuth();
+  const allowEdit = user && user.id === userAcc.id;
 
-const CardProfileSetting = () => <Tab menu={{ secondary: true }} panes={panes} />
+  const [username, setUsername] = useState(userAcc.username)
+
+  const renderPanes = () => {
+    const Update = async () => {
+      if (!user)
+        router.push(`/sign-in?forward=${encodeURIComponent(router.asPath)}`);
+      else {
+        await Client(`user/${userAcc.id}`, "PATCH", {
+          username,
+          email,
+          avatarUrl,
+          password,
+          oldPassword,
+        }).then(({ data }) => {
+          console.log("L21", data);
+          setTitle("");
+          setContent("");
+          setTags([""]);
+          router.push("/questions");
+        });
+      }
+    };
+    return [
+      {
+        menuItem: "Change Profile",
+        render: () => (
+          <Tab.Pane attached={false}>
+            <Form unstackable>
+              <Form.Group widths={2}>
+                <Form.Input
+                  label="Email"
+                  placeholder="email"
+                  value="abc"
+                  disabled
+                />
+                <Form.Input
+                  label="Username"
+                  placeholder="username"
+                  value={username}
+                  disabled
+                />
+              </Form.Group>
+              <Form.Group widths={2}>
+                <Form.Input label="Name" placeholder="name" />
+                <Form.Input label="Phone number" placeholder="phone" />
+              </Form.Group>
+              <Form.Field>
+                <Button
+                  content="Choose Image"
+                  labelPosition="left"
+                  icon="image"
+                  onClick={() => fileInputRef.current.click()}
+                />
+                <input
+                  ref={fileInputRef}
+                  type="file"
+                  hidden
+                  // onChange={this.fileChange}
+                />
+              </Form.Field>
+              <Button color="linkedin" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Tab.Pane>
+        ),
+      },
+      {
+        menuItem: "Change Password",
+        render: () => (
+          <Tab.Pane attached={false}>
+            <Form>
+              <Form.Field required>
+                <label>Old Password</label>
+                <Input placeholder="old pass" required />
+              </Form.Field>
+              <Form.Field required>
+                <label>New Password</label>
+                <Input placeholder="new pass" required />
+              </Form.Field>
+              <Form.Field required>
+                <label>Repeat New Password</label>
+                <Input placeholder="repeat new pass" required />
+              </Form.Field>
+              <Button color="linkedin" type="submit">
+                Confirm
+              </Button>
+            </Form>
+          </Tab.Pane>
+        ),
+      },
+    ];
+  };
+
+  const [panes, setPanes] = useState([]);
+
+  useEffect(() => {
+    renderPanes().then((components) => setPanes(components));
+  }, [allowEdit]);
+
+  return <Tab menu={{ secondary: true }} panes={panes} />;
+};
 
 export default CardProfileSetting;

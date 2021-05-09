168877167e12657f74aef237f06b19ebe8eabcf4
tranphuquy19 <christian@enclave.vn>
2021-05-09 13:49:29 +0700

    update

--- git-author.sh
+++ git-author.sh
@@ -6,6 +6,4 @@ CORRECT_NAME="tranphuquy19"
 CORRECT_EMAIL="christian@enclave.vn"
 export GIT_COMMITTER_NAME="$CORRECT_NAME"
 export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 ' --tag-name-filter cat -- --branches --tags

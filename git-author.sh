4630b087236b749766b91a4d7b9a14d7ea450e7c
tranphuquy19 <christian@enclave.vn>
2021-05-09 13:46:36 +0700

    update

--- git-author.sh
+++ git-author.sh
@@ -4,7 +4,8 @@ git filter-branch --env-filter '
 OLD_EMAIL="chrisian@enclave.vn"
 CORRECT_NAME="tranphuquy19"
 CORRECT_EMAIL="christian@enclave.vn"
-
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 export GIT_AUTHOR_NAME="$CORRECT_NAME"
 export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 ' --tag-name-filter cat -- --branches --tags
